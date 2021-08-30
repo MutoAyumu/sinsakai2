7fbbe3c1f7f96876b4113453a2cfb96749641d82
MutoAyumu <mutoayum0610@gmail.com>
2021-08-30 20:25:19 +0900

    マップにプレイヤーとボスのスプライトを追加

--- Assets/script/SystemScript/load.cs
+++ Assets/script/SystemScript/load.cs
@@ -2,34 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class load : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>ロードするシーン名</summary>
+    [SerializeField] string m_sceneNameToBeLoaded = "SceneNameToBeLoaded";
+    /// <summary>フェードするためのマスクとなる Panel (Image) を指定する。アルファを 0 にしておき、必要ならば Raycast Target をオフにしておくこと。</summary>
+    [SerializeField] Image m_fadePanel = null;
+    /// <summary>フェードするスピード</summary>
+    [SerializeField] float m_fadeSpeed = 1f;
+    /// <summary>ロード開始フラグ</summary>
+    bool m_isLoadStarted = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void LoadSceneEnd()
-    {
-        SceneManager.LoadScene("EndScene");
-        //UnityEngine.SceneManagement.SceneManager.LoadScene("EndScene", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+        // ロードが開始されたら
+        if (m_isLoadStarted)
+        {
+            // m_panel が設定されていたら、徐々にアルファを上げて不透明にする
+            if (m_fadePanel)
+            {
+                Color panelColor = m_fadePanel.color;
+                panelColor.a += m_fadeSpeed * Time.deltaTime;
+                m_fadePanel.color = panelColor;
+
+                // ほぼ不透明になったら
+                if (panelColor.a > 0.99f)
+                {
+                    // シーンをロードする
+                    SceneManager.LoadScene(m_sceneNameToBeLoaded);
+                    m_isLoadStarted = false;
+                }
+            }
+            else
+            {
+                // m_panel が設定されていない時は、すぐにシーンをロードする
+                SceneManager.LoadScene(m_sceneNameToBeLoaded);
+                m_isLoadStarted = false;
+            }
+        }
     }
 
-    public void LoadSceneStart()
+    /// <summary>
+    /// シーンをロードする
+    /// </summary>
+    public void LoadScene()
     {
-        SceneManager.LoadScene("TitleScene");
-    }
-
-    public void LoadRestart()
-    {
-        SceneManager.LoadScene("GameScene");
-        Time.timeScale = 1f;
+        m_isLoadStarted = true;
     }
 }
