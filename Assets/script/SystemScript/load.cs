ce5aa37bcfc787e1e617b4737c5c767698250f19
MutoAyumu <mutoayum0610@gmail.com>
2021-09-08 20:33:22 +0900

    終了ボタンの追加・タイトルボタンの完成

--- Assets/script/SystemScript/load.cs
+++ Assets/script/SystemScript/load.cs
@@ -54,6 +54,10 @@ public class load : MonoBehaviour
 
     public void Exit()
     {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
