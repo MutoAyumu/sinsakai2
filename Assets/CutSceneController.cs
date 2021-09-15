072c6ee229059bec57a85c93c7fe263bbb610e9d
MutoAyumu <mutoayum0610@gmail.com>
2021-09-15 18:48:15 +0900

    ボス登場のカットSceneを作った

---
+++ Assets/CutSceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CutSceneController : MonoBehaviour
+{
+    [SerializeField] UnityEvent m_eventOnCutSceneEnds = default;
+
+    public void OnCutSceneEnd()
+    {
+        m_eventOnCutSceneEnds.Invoke();
+    }
+}
